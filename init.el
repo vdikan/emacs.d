3512c61783c2fefa304d1b86b7801f67d150ccbb
Vladimir <vladimir.dikan@icn2.cat>
2018-10-22 18:21:24 +0200

    mode-line color style

--- init.el
+++ init.el
@@ -54,6 +54,11 @@
   :init
   (put 'narrow-to-region 'disabled nil)
   (put 'downcase-region 'disabled nil)
+  (set-face-attribute 'default nil :height 180)
+  (set-face-attribute 'mode-line           nil :background "dark slate blue" :foreground "gainsboro")
+  (set-face-attribute 'mode-line-buffer-id nil :background "RoyalBlue3" :foreground "white smoke")
+  (set-face-attribute 'mode-line-highlight nil :box nil :background "steel blue" :foreground "white")
+  (set-face-attribute 'mode-line-inactive  nil :inherit 'default)
   :custom
   (scroll-step 1)
   (inhibit-startup-screen t "Don't show splash screen")
@@ -72,7 +77,9 @@
   (sentence-end-double-space nil)
   (default-fill-column 80)
   (initial-scratch-message ";;; Good morning, Captain!\n\n")
-  (debug-on-quit nil))
+  (debug-on-quit nil)
+  (column-number-mode 1)
+  )
 
 ;;; Appearance
 (blink-cursor-mode 0)
@@ -174,6 +181,9 @@
                 (setq comment-start "!!")
                 ;; f90-directive-comment-re "!!$"
                 ;; f90-indented-comment-re "!!"
+                ;; (generate-fortran-tags)
+                (setq fortran-tags-path  ;; doesnt work?
+                      (concat (projectile-project-root) "FORTAGS"))
                 )))
 
 ;; TODO fork for quelpa
