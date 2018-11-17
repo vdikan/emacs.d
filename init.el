7c22065f13239f418fb065852f8cda5b1c5c9a08
Vladimir <dikan.va@ya.ru>
2018-11-17 01:25:40 +0100

    pdf-tools

--- init.el
+++ init.el
@@ -190,6 +190,19 @@
   (org-mode . org-bullets-mode))
 
 
+(use-package pdf-tools
+  :pin manual ;; manually update
+  :config
+  ;; initialise
+  (pdf-tools-install)
+  ;; open pdfs scaled to fit page
+  (setq-default pdf-view-display-size 'fit-page)
+  ;; automatically annotate highlights
+  (setq pdf-annot-activate-created-annotations t)
+  ;; use normal isearch
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+
+
 (use-package ivy-bibtex
   :ensure t
   :custom
